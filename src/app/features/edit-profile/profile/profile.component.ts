613cb3fcd0b403bfec5d18155b1c4f3c5bbdb71a
Kane <95705546+kaneran@users.noreply.github.com>
2025-02-05 22:13:36 +0000

    Initial setup for Profile component

--- src/app/features/edit-profile/profile/profile.component.ts
+++ src/app/features/edit-profile/profile/profile.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  formBuilder: FormBuilder = inject(FormBuilder);
+  profileForm: FormGroup;
 
+  constructor() {
+    this.profileForm = this.formBuilder.group({
+      quantity: this.formBuilder.control('', [Validators.required, Validators.min(1)]),
+      type: this.formBuilder.control('', Validators.required),
+      age: this.formBuilder.control('', [Validators.required, Validators.min(16)])
+    }
+    );
+  }
 }
