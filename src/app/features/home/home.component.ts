0476d5adf047f118a817c90abe6f22e6155aa753
Kane <95705546+kaneran@users.noreply.github.com>
2025-04-04 07:38:28 +0100

    Initial setup of Sign in modal

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../../shared/components/modal/modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  doShowLoginModal: boolean = true;
 }
