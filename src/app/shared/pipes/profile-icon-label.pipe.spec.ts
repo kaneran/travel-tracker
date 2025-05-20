641fba1ac173a6b83882bc9c4ffa4c5fbba0f853
Kane <95705546+kaneran@users.noreply.github.com>
2025-05-20 08:08:07 +0100

    Updated profile icon to include first letters of user's name + Updated View profile page to display appropriate info to first time users

---
+++ src/app/shared/pipes/profile-icon-label.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ProfileIconLabelPipe } from './profile-icon-label.pipe';
+
+describe('ProfileIconLabelPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProfileIconLabelPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
